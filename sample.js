6fd40fa3c2cb1be9a7921f5fced4ec5828fe4853
Andisheh1975 <Andisheh.saffarian@students.mq.edu.au>
2022-03-25 11:17:14 +1100

    update the files

---
+++ sample.js
@@ -0,0 +1,27 @@
+
+
+
+/*let btn1 = document.querySelector('#btn1')
+
+let btn2 = document.querySelector('#btn2')
+
+let btn3 = document.querySelector('#3')
+
+let btn4 = document.querySelector('#4')
+
+btn1.addEventListener('click',() =>btn1.getElementsByClassName.backgroundColor= '#337ab7')
+btn2.addEventListener('click',() =>btn2.getElementsByClassName.backgroundColor= '#c9302c')
+btn3.addEventListener('click',() =>btn3.getElementsByClassName.backgroundColor= '#4cae4c')
+btn4.addEventListener('click',() =>btn4.getElementsByClassName.backgroundColor= '#337ab7')*/
+
+const first = document.getElementById(btn1);
+
+first.style = 'background-color: #337ab7' + '#337ab7';
+
+
+
+
+
+
+
+
